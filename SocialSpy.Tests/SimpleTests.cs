aa444dd11a5ec5414551128809ceb2f15647108c
Sequenta <tranquilized.mind@gmail.com>
2013-07-03 19:28:50 +0600

    Тест для проверки выполнения на билд сервере

---
+++ SocialSpy.Tests/SimpleTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SocialSpy.Controllers;
+
+namespace SocialSpy.Tests
+{
+    [TestClass]
+    public class SimpleTests
+    {
+        [TestMethod]
+        public void HomeController_ReturnsCorrectValue()
+        {
+            var controller = new HomeController();
+
+            var value = controller.Index();
+
+            Assert.AreEqual("HelloWorld!",value);
+        }
+    }
+}
