67ff49fb0f7aab8bdc65fd5b1c8e967a09f56fc8
Sequenta <tranquilized.mind@gmail.com>
2013-07-08 17:55:04 +0600

    Добавлен метод, возвращающий информацию о пользователе

--- SocialSpy/Models/UserInfo.cs
+++ SocialSpy/Models/UserInfo.cs
@@ -1,7 +1,23 @@
-﻿namespace SocialSpy.Models
+﻿using System.IO;
+using Microsoft.AspNet.SignalR.Client.Http;
+
+namespace SocialSpy.Models
 {
     public class UserInfo
     {
-         
+        public string GetUserInfo(string id)
+        {
+            var http = new DefaultHttpClient();
+            var requestUrl = string.Format("https://api.vk.com/method/getProfiles?uids={0}&fields=sex,city,country", id);
+            var jsonResult = "";
+            using (var result = http.Post(requestUrl, request => { }).Result)
+            {
+                using (var resultStream = new StreamReader(result.GetStream()))
+                {
+                    jsonResult = resultStream.ReadToEnd();
+                }
+            }
+            return jsonResult;
+        } 
     }
 }
