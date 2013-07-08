a823d578138905ca4d2c158622a02bae66a75929
Sequenta <tranquilized.mind@gmail.com>
2013-07-09 01:48:19 +0600

    Перенесена функция из UserInfo

--- SocialSpy/Controllers/HomeController.cs
+++ SocialSpy/Controllers/HomeController.cs
@@ -1,21 +1,60 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web.Mvc;
+using Microsoft.AspNet.SignalR.Client.Http;
+using Microsoft.AspNet.SignalR.Json;
 using SocialSpy.Models;
+
 
 namespace SocialSpy.Controllers
 {
     public class HomeController : Controller
     {
-        UserInfo userInfo = new UserInfo();
         public ViewResult Index()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult GetUserInfo(string request)
+        public ViewResult GetUserInfo(string id)
         {
-            var info = userInfo.GetUserInfo(request);
-            return Json(info, JsonRequestBehavior.AllowGet);
+            var http = new DefaultHttpClient();
+            var requestUrl = string.Format("https://api.vk.com/method/getProfiles?uids={0}&fields=sex,city,country,photo_200_orig", id);
+            UserInfo userInfo;
+            using (var result = http.Post(requestUrl, request => { }).Result)
+            {
+                using (var resultStream = new StreamReader(result.GetStream()))
+                {
+                    var jsonResult = resultStream.ReadToEnd();
+                    userInfo =  CreateUserInfo(jsonResult);
+                }
+            }
+            return View("UserInfo",userInfo);
+        }
+
+        private UserInfo CreateUserInfo(string jsonResult)
+        {
+            jsonResult = jsonResult.Replace(@"{""response"":[", "");
+            jsonResult = jsonResult.Remove(jsonResult.Length - 2);
+            var serializer = new JsonNetSerializer();
+            var values = serializer.Parse<Dictionary<string, string>>(jsonResult);
+
+            var userInfo = new UserInfo();
+            userInfo.UserId = values["uid"];
+            userInfo.FirstName = values["first_name"];
+            userInfo.LastName = values["last_name"];
+            userInfo.Sex = TryGetValue("sex",values);
+            userInfo.City = TryGetValue("city", values);
+            userInfo.Country = TryGetValue("country", values);
+            userInfo.Image = TryGetValue("photo_200_orig", values);
+            return userInfo;
+        }
+
+        private string TryGetValue(string valueName, Dictionary<string, string> values)
+        {
+            string outValue;
+            values.TryGetValue(valueName, out outValue);
+            return outValue;
         }
     }
 }
