62aef9523ab63aa85177feeacf7deab5eb41555d
Sequenta <tranquilized.mind@gmail.com>
2013-07-10 19:51:14 +0600

    Hub для получения информации о пользователях ВК

---
+++ SocialSpy/Hubs/VkUserInfoHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR;
+using SocialSpy.Informers;
+
+namespace SocialSpy.Hubs
+{
+    public class VkUserInfoHub:Hub
+    {
+        public void GetUserInfo(string id)
+        {
+            var informer = new VkInformer();
+            var info = informer.GetUserInfo(id);
+            Clients.All.viewUserInfo(info);
+        }
+    }
+}
