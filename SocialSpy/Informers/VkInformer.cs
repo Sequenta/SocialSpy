a91a1fb4242aff3b0bdca25d34f42e8cb09cbce7
Sequenta <tranquilized.mind@gmail.com>
2013-07-10 19:44:00 +0600

    Добавлен метод получения информации о пользователе

--- SocialSpy/Informers/VkInformer.cs
+++ SocialSpy/Informers/VkInformer.cs
@@ -1,7 +1,23 @@
-﻿namespace SocialSpy.Informers
+﻿using System.IO;
+using Microsoft.AspNet.SignalR.Client.Http;
+
+namespace SocialSpy.Informers
 {
     public class VkInformer
     {
-         
+        public string GetUserInfo(string id)
+        {
+            var http = new DefaultHttpClient();
+            var requestUrl = string.Format("https://api.vk.com/method/getProfiles?uids={0}&fields=sex,city,country,photo_200_orig", id);
+            string userInfo;
+            using (var result = http.Post(requestUrl, request => { }).Result)
+            {
+                using (var resultStream = new StreamReader(result.GetStream()))
+                {
+                    userInfo = resultStream.ReadToEnd();
+                }
+            }
+            return userInfo;
+        } 
     }
 }
