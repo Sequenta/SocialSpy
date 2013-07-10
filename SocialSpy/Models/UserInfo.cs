bb4044a973953c30849e5aa2c53a4365641ecd0f
Sequenta <tranquilized.mind@gmail.com>
2013-07-10 19:35:22 +0600

    Добавлен конструктор и вспомогательный метод класса UserInfo

--- SocialSpy/Models/UserInfo.cs
+++ SocialSpy/Models/UserInfo.cs
@@ -1,4 +1,7 @@
-﻿namespace SocialSpy.Models
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.SignalR.Json;
+
+namespace SocialSpy.Models
 {
     public class UserInfo
     {
@@ -9,5 +12,27 @@
         public string City { get; set; }
         public string Country { get; set; }
         public string Image { get; set; }
+
+        public UserInfo(string jsonParameters)
+        {
+            jsonParameters = jsonParameters.Replace(@"{""response"":[", "");
+            jsonParameters = jsonParameters.Remove(jsonParameters.Length - 2);
+            var serializer = new JsonNetSerializer();
+            var values = serializer.Parse<Dictionary<string, string>>(jsonParameters);
+            UserId = values["uid"];
+            FirstName = values["first_name"];
+            LastName = values["last_name"];
+            Sex = TryGetValue("sex", values);
+            City = TryGetValue("city", values);
+            Country = TryGetValue("country", values);
+            Image = TryGetValue("photo_200_orig", values);
+        }
+
+        private string TryGetValue(string valueName, Dictionary<string, string> values)
+        {
+            string outValue;
+            values.TryGetValue(valueName, out outValue);
+            return outValue;
+        }
     }
 }
