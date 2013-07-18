f65f2d1b203f3c061482142acf89af5b87251fc5
Sequenta <tranquilized.mind@gmail.com>
2013-07-18 15:49:08 +0600

    Тесты для модели

---
+++ SocialSpy.Tests/ModelTests/UserInfoTests.cs
@@ -0,0 +1,17 @@
+﻿using SocialSpy.Models;
+using Xunit;
+
+namespace SocialSpy.Tests.ModelTests
+{
+    public class UserInfoTests
+    {
+        [Fact]
+        public void UserInfo_ReturnsEmptyInstance_WhenJsonStringIsEmpty()
+        {
+            var jsonString = "";
+            var userInfo = new UserInfo(jsonString);
+
+            Assert.Equal(new UserInfo(),userInfo);
+        } 
+    }
+}
