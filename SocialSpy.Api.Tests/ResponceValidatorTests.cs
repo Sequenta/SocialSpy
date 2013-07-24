66d4f0bd51dac5c384f54c9923954064e31b7ceb
Sequenta <tranquilized.mind@gmail.com>
2013-07-24 16:58:56 +0600

    Тесты для ResponseValidator

---
+++ SocialSpy.Api.Tests/ResponceValidatorTests.cs
@@ -0,0 +1,32 @@
+﻿using SocialSpy.Api.VK;
+using Xunit;
+
+namespace SocialSpy.Api.Tests
+{
+    public class ResponceValidatorTests
+    {
+        ResponseValidator validator = new ResponseValidator();
+
+        [Fact]
+        public void ReturnsDataWithoutResponseTag()
+        {
+            var result = validator.RemoveResponseTag("response: [{uid: 1,first_name: 'Павел',last_name: 'Дуров'}]");
+            Assert.Equal("{uid: 1,first_name: 'Павел',last_name: 'Дуров'}",result);
+        }
+
+        [Fact]
+        public void ReturnsValidResponseData()
+        {
+            var result = validator.GetResponseData("response: [{uid: 1,first_name: 'Павел',last_name: 'Дуров'}]");
+            Assert.Equal("1",result["uid"]);
+            Assert.Equal("Павел",result["first_name"]);
+            Assert.Equal("Дуров",result["last_name"]);
+        }
+
+        [Fact]
+        public void ThrowsEmptyResponseException()
+        {
+            Assert.Throws<EmptyResponseException>(() => validator.GetResponseData(""));
+        }
+    }
+}
---
+++ SocialSpy.Api/VK/ResponseValidator.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace SocialSpy.Api.VK
+{
+    public class ResponseValidator
+    {
+        public JObject GetResponseData(string responseJson)
+        {
+            if (responseJson == string.Empty)
+            {
+                throw new EmptyResponseException();
+            }
+            var responseData = string.Empty;
+            if (responseJson.Contains("response"))
+            {
+                responseData = RemoveResponseTag(responseJson);
+            }
+            return JObject.Parse(responseData);
+        }
+
+        public string RemoveResponseTag(string responseJson)
+        {
+            var responseData = responseJson.Replace(@"response: [", "");
+            responseData = responseData.Remove(responseData.Length - 1);
+            return responseData;
+        }
+    }
+}
